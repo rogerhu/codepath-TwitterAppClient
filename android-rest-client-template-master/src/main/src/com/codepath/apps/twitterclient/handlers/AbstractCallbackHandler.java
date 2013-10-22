d71682b6a3488881e331b63db75d8516888bfb83
Roger Hu <roger.hu@gmail.com>
2013-10-22 01:17:35 -0700

    Get rid of extra files.

--- android-rest-client-template-master/src/main/src/com/codepath/apps/twitterclient/handlers/AbstractCallbackHandler.java
+++
@@ -1,8 +0,0 @@
-package com.codepath.apps.twitterclient.handlers;
-
-/**
- * Created by rhu on 10/20/13.
- */
-public interface AbstractCallbackHandler<T> {
-    public abstract void processItem(T item);
-}
